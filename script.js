3cfb8d461c944464c949c8d95015e0e7bbb8b5bb
Mattia <mattiabunel@hotmail.fr>
2017-05-04 12:31:36 +0200

    Extraction js
    + Correction style tableau

---
+++ script.js
@@ -0,0 +1,106 @@
+"use strict";
+
+var map = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles',
+    subdomains: 'abcd',
+    minZoom: 1,
+    maxZoom: 16,
+    ext: 'png'
+}).addTo(map);
+
+
+var markersGroup = new L.featureGroup()
+    .addTo(map);
+
+function onMapClick(e) {
+    "use strict";
+    var newMarlef = new L.marker(e.latlng, {
+	// marker déplacable
+	draggable: true
+    })
+    // Ajout au tableau
+	.on('add',
+	    function (e) {
+		var latLng = e.target.getLatLng(),
+		    lat = latLng.lat.toFixed(2),
+		    lon = latLng.lng.toFixed(2);
+		console.log([lat, lon]);
+		// TODO
+		markerSidebar([lat, lon]);
+	    }
+	   )
+    // Suppression marker clic droit
+	.on('contextmenu',
+	    function () {
+		markersGroup.removeLayer(this);
+	    }
+	   )
+    // Changement type au clic
+	.on('click',
+	    function (e) {
+		console.log(e);
+		// TODO
+	    }
+	   )
+	.addTo(markersGroup);
+}
+
+function markerList() {
+    var allMarkersObjArray = [];
+    var allMarkersGeoJsonArray = [];
+
+    $.each(map._layers, function (ml) {
+        //console.log(map._layers)
+        if (map._layers[ml]._layers) {
+
+	    $.each(this._layers, function () {
+		allMarkersObjArray.push(this);
+		allMarkersGeoJsonArray.push(JSON.stringify(this.toGeoJSON()));
+	    });
+	}
+    });
+
+    console.log(allMarkersGeoJsonArray);
+}
+
+
+function onMarkerLeftClick(e) {
+    /* var newPopup = new L.popup()
+       .setLatLng(e.latlng)
+       .setContent("Popup")
+       .openOn(map);
+    */
+}
+
+map.on('click', onMapClick);
+
+
+// Add marker
+function markerSidebar(a) {
+    $("#feature-list tbody")
+	.append($('<tr>')
+		.addClass("feature-row")
+		.append($('<td>').text(a[0]))
+		.append($('<td>').text(a[1]))
+	       );
+}
+
+$("#sidebar-toggle-btn").click(function() {
+    animateSidebar();
+    return false;
+});
+
+$("#sidebar-hide-btn").click(function() {
+    animateSidebar();
+    return false;
+});
+
+function animateSidebar() {
+    $("#sidebar").animate({
+	width: "toggle"
+    }, 350, function() {
+	map.invalidateSize();
+    });
+}
