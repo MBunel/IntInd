f3aa502dc704f2038ca322c219019b9c39dce494
Mattia <mattiabunel@hotmail.fr>
2017-06-26 14:53:47 +0200

    Ajout interactions figure > tableau

--- Acc/static/Acc/interaction.js
+++ Acc/static/Acc/interaction.js
@@ -76,7 +76,79 @@ $(function() {
 	{
 	    'orig': "div.table-responsive:nth-child(3) > table:nth-child(1) > caption:nth-child(2)",
 	    'dest': ["#g-alpha > *", "#g-gamma > *", "#g-mu > *"]
+	},
+
+
+
+	{
+	    'orig': "#rect-qt",
+	    'dest': ["div.table-responsive:nth-child(1) > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(1)"]
+	},
+	{
+	    'orig': "#rect-bt",
+	    'dest': ["div.table-responsive:nth-child(1) > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(2)"]
+	},
+	{
+	    'orig': "#rect-rt",
+	    'dest': ["div.table-responsive:nth-child(1) > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(3)"]
+	},
+	{
+	    'orig': "#rect-ct",
+	    'dest': ["div.table-responsive:nth-child(1) > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(4)"]
+	},
+	{
+	    'orig': "#rect-pt",
+	    'dest': ["div.table-responsive:nth-child(1) > table:nth-child(1) > tbody:nth-child(2) > tr:nth-child(5)"]
+	},
+
+
+
+	{
+	    'orig': "#path-b1",
+	    'dest': ["div.table-responsive:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1)"]
+	},
+	{
+	    'orig': "#path-b2",
+	    'dest': ["div.table-responsive:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2)"]
+	},
+	{
+	    'orig': "#path-c1",
+	    'dest': ["div.table-responsive:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3)"]
+	},
+	{
+	    'orig': "#path-c2",
+	    'dest': ["div.table-responsive:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(4)"]
+	},
+	{
+	    'orig': "#path-s1",
+	    'dest': ["div.table-responsive:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(5)"]
+	},
+	{
+	    'orig': "#path-s2",
+	    'dest': ["div.table-responsive:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(6)"]
+	},
+	{
+	    'orig': "#path-gammat",
+	    'dest': ["div.table-responsive:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(7)"]
+	},
+	{
+	    'orig': "#path-phit",
+	    'dest': ["div.table-responsive:nth-child(2) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(8)"]
+	},
+
+	{
+	    'orig': "#g-alpha > *",
+	    'dest': ["div.table-responsive:nth-child(3) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1)"]
+	},
+	{
+	    'orig': "#g-gamma > *",
+	    'dest': ["div.table-responsive:nth-child(3) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2)"]
+	},
+	{
+	    'orig': "#g-mu > *",
+	    'dest': ["div.table-responsive:nth-child(3) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3)"]
 	}
+
     ];
 
     a.map(function(x) {
