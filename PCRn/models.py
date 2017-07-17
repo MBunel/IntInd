0bcdf69d683c324f8b99ab39c3cd4898ed4a3784
Mattia <mattiabunel@hotmail.fr>
2017-07-17 18:26:30 +0200

    Ajout de tables pour intégrer les résultats des simulations dans la base

--- PCRn/models.py
+++ PCRn/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Simulation(models.Model):
+    timestamp = models.TimeField()
+
+
+class Parameters(models.Model):
+    simulation = models.ForeignKey(Simulation)
+    parameter = models.CharField(blank=False)
+    value = models.FloatField(blank=False)
+
+
+class Node(models.Model):
+    simulation = models.ForeignKey(Simulation)
+
+
+class Link(models.Model):
+    simulation = models.ForeignKey(Simulation)
+    idA = models.ForeignKey(Node, related_name="idA")
+    idB = models.ForeignKey(Node, related_name="idB")
+
+
+class Results(models.Model):
+    simulation = models.ForeignKey(Simulation)
+    time = models.FloatField(blank=False)
+    variable = models.CharField(blank=False)
+    value = models.FloatField(blank=False)
