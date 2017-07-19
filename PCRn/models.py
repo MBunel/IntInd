419d9a7f7c6e1d2262d4a57fd2994c838ed15547
Mattia <mattiabunel@hotmail.fr>
2017-07-19 15:08:07 +0200

    Ajout des contraintes ON DELETE

--- PCRn/models.py
+++ PCRn/models.py
@@ -8,25 +8,27 @@ class Simulation(models.Model):
 
 
 class Parameters(models.Model):
-    simulation = models.ForeignKey(Simulation)
+    simulation = models.ForeignKey(Simulation, on_delete=models.CASCADE)
     parameter = models.CharField(blank=False, max_length=20)
     value = models.FloatField(blank=False)
 
 
 class Node(models.Model):
-    simulation = models.ForeignKey(Simulation)
+    simulation = models.ForeignKey(Simulation, on_delete=models.CASCADE)
     m_id = models.IntegerField()
 
 
 class Edge(models.Model):
-    simulation = models.ForeignKey(Simulation)
-    idA = models.ForeignKey(Node, related_name="idA")
-    idB = models.ForeignKey(Node, related_name="idB")
+    simulation = models.ForeignKey(Simulation, on_delete=models.CASCADE)
+    idA = models.ForeignKey(Node, related_name="idA",
+                            on_delete=models.CASCADE)
+    idB = models.ForeignKey(Node, related_name="idB",
+                            on_delete=models.CASCADE)
 
 
 class Results(models.Model):
-    simulation = models.ForeignKey(Simulation)
-    node = models.ForeignKey(Node)
+    simulation = models.ForeignKey(Simulation, on_delete=models.CASCADE)
+    node = models.ForeignKey(Node, on_delete=models.CASCADE)
     time = models.FloatField(blank=False)
     dr = models.FloatField(blank=False)
     dc = models.FloatField(blank=False)
