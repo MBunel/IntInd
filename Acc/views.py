13a9bc5d7456aae03c1fb047632441c4fa140cf2
Mattia <mattiabunel@hotmail.fr>
2017-07-24 17:10:49 +0200

    Écriture page "À propos"

--- Acc/views.py
+++ Acc/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
 
 # Create your views here.
@@ -6,3 +6,13 @@ def index(request):
     return render(request,
                   'Acc/index.xhtml',
                   {'projectName': 'projet'})
+
+
+def aprp(request):
+    return render(request,
+                  'Acc/apropos.xhtml',
+                  {'projectName': 'projet'})
+
+
+def rickroll(request):
+    return HttpResponseRedirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
