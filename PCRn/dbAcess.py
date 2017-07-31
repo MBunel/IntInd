23d5f26b0c5ba4d9a4e542ef62833b402de516f1
Mattia <mattiabunel@hotmail.fr>
2017-07-31 16:23:56 +0200

    Base écriture du nouveau parseur

---
+++ PCRn/dbAcess.py
@@ -0,0 +1,101 @@
+import numpy as np
+from django.db import transaction
+from itertools import chain
+from PCRn.models import Simulation, Node, Edge, Results, Network
+
+
+class dbAcess:
+
+    def __init__(self, data):
+        print(__name__)
+        self.dbWrite("a", "b")
+
+    @transaction.atomic
+    def dbWrite(self, nodeList, res):
+        # Si on crée un nv réseau
+        if True:
+            network = self.networkWrite("netname")
+        # Sinon on renvoie l'id du précédent
+        else:
+            network = "id"
+
+        sim = self.simWrite(network)
+        # self.parmsWrite(simId)
+        nodes = self.nodesWrite(sim, nodeList)
+        self.resWirte(sim, nodes, self.time, res)
+
+    def networkWrite(self, name):
+        net = Network(title=name)
+        net.save()
+        return net
+
+    def _hWrite(self):
+        print(__name__)
+
+    def HWrite(self):
+        print(__name__)
+
+    def FWrite(self):
+        print(__name__)
+
+    def GWrite(self):
+        print(__name__)
+
+    def PcrWrite(self):
+        print(__name__)
+
+    def nodesWrite(self, sim, nodeList):
+        nodes = []
+        for i in nodeList:
+            node = Node(simulation=sim, m_id=i)
+            node.save()
+            nodes.append(node)
+        return nodes
+
+    def linComWrite(self):
+        print(__name__)
+
+    def QuadComWrite(self):
+        print(__name__)
+
+    def edgesWrite(self, sim, nodeList, edgeList):
+        edges = [Edge(idA=0, idB=1) for i in edgeList]
+        Edge.objects.bulk_create(edges)
+
+    def simWrite(self):
+        sim = Simulation()
+        sim.save()
+        return sim
+
+    def resWirte(self, sim, nodes, time, res):
+        # On transpose la matrice dans un format
+        # où les lignes sont les durées et les colones
+        # les couples noeuds/valeurs
+        resT = np.transpose(res)
+        temp = []
+        # Pour chaque ligne
+        for r in range(len(resT)):
+            # On récupère le temps associé
+            tvalue = time[r]
+            # TODO: Remplacer 4 par N valeurs
+            # On groupe les colones par noeuds
+            # toutes les 4 valeurs on a un nouveau noeud
+            resG = zip(*(iter(resT[r]),)*4)
+            # On crée une liste de Results pour chaque noeuds
+            resOb = (Results(simulation=sim,
+                             node=nodes[i],
+                             time=tvalue,
+                             dr=v[0], dc=v[1], dp=v[2],
+                             dq=v[3]) for i, v in enumerate(resG))
+            # On chaine les itérables pour créer une longue chaine de
+            # générateurs. Le calcul n'est pas effectué
+            temp = chain(temp, resOb)
+        # On entre les données massivement dans la base.
+        Results.objects.bulk_create(temp)
+
+    def DBRead(self):
+        print(__name__)
+
+    def SimulationList(self):
+        sl = Simulation.objects.all()
+        return sl
